752e355f9cb73d7c9eec1800c7fb6e141cd8a040
Tomas Bellus <tomas.bellus@gmail.com>
2020-02-16 22:50:16 +0100

    create Servo library wrapper

---
+++ lib/Servo_motor/Servo_motor.cpp
@@ -0,0 +1,18 @@
+#include "Servo_motor.hpp"
+
+Servo_motor::Servo_motor(const int leftmost_position_degree, const int rightmost_position_degree)
+        : leftmost_pos_deg(leftmost_position_degree), rightmost_pos_deg(rightmost_position_degree) {
+    this->servo = new Servo();
+}
+
+int Servo_motor::leftmost_position() {
+    if (not this->servo->attached()) return SERVO_IS_NOT_ATTACHED;
+
+    this->servo->write(this->leftmost_pos_deg);
+}
+
+int Servo_motor::rightmost_position() {
+    if (not this->servo->attached()) return SERVO_IS_NOT_ATTACHED;
+
+    this->servo->write(this->rightmost_pos_deg);
+}
---
+++ lib/Servo_motor/Servo_motor.hpp
@@ -0,0 +1,23 @@
+#ifndef SERVO_SERVO_MOTOR_H
+#define SERVO_SERVO_MOTOR_H
+
+#include <Servo.h>
+
+// ERROR CODES
+static const int SERVO_IS_NOT_ATTACHED = 1;
+
+class Servo_motor: Servo {
+private:
+    Servo *servo = nullptr;
+    const int leftmost_pos_deg;
+    const int rightmost_pos_deg;
+public:
+    Servo_motor(const int rightmost_position_degree, const int leftmost_position_degree);
+    ~Servo_motor();
+    int rightmost_position();
+    int leftmost_position();
+    int get_position_degree();
+};
+
+
+#endif
