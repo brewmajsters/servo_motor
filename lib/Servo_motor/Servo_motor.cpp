bc860682a0f88535945db50a4644b4e46d922834
Tomas Bellus <tomas.bellus@gmail.com>
2020-04-20 17:04:29 +0200

    get use of inheritence and implement poll rating

--- lib/Servo_motor/Servo_motor.cpp
+++ lib/Servo_motor/Servo_motor.cpp
@@ -1,36 +1,38 @@
 #include "Servo_motor.hpp"
 
-Servo_motor::Servo_motor(const std::string uuid, const uint8_t pin, const uint16_t poll_rate,
+Servo_motor::Servo_motor(std::string uuid, const uint8_t pin, const uint16_t poll_rate,
                          const uint8_t leftmost_position_degree,
                          const uint8_t rightmost_position_degree)
-    : uuid(uuid), pin(pin), poll_rate(poll_rate), leftmost_pos_deg(leftmost_position_degree),
-      rightmost_pos_deg(rightmost_position_degree) {
-  this->servo = Servo();
-  this->servo.attach(pin);
+    : Servo(), uuid(std::move(uuid)), poll_rate(poll_rate), leftmost_pos_deg(leftmost_position_degree),
+      rightmost_pos_deg(rightmost_position_degree), wait(poll_rate) {
+  this->attach(pin);
 }
 
-const std::string Servo_motor::get_uuid() {
+std::string Servo_motor::get_uuid() {
   return this->uuid;
 }
 
-uint8_t Servo_motor::get_pin() {
-  if (servo.attached()) return (uint8_t) this->servo.attachedPin();
-}
-
 uint8_t Servo_motor::move_to_leftmost_position() {
-  if (not this->servo.attached()) return SERVO_IS_NOT_ATTACHED;
+  if (not this->attached()) return PIN_NOT_ATTACHED;
 
-  this->servo.write(this->leftmost_pos_deg);
+  this->write(this->leftmost_pos_deg);
 }
 
 uint8_t Servo_motor::move_to_rightmost_position() {
-  if (not this->servo.attached()) return SERVO_IS_NOT_ATTACHED;
+  if (not this->attached()) return PIN_NOT_ATTACHED;
 
-  this->servo.write(this->rightmost_pos_deg);
+  this->write(this->rightmost_pos_deg);
 }
 
 uint8_t Servo_motor::get_current_position() {
-  if (not this->servo.attached()) return SERVO_IS_NOT_ATTACHED;
+  if (not this->attached()) return PIN_NOT_ATTACHED;
 
-  return this->servo.read();
+  return this->read();
+}
+
+bool Servo_motor::poll() {
+  if (--this->wait) return false;
+
+  this->wait = this->poll_rate;
+  return true;
 }
--- lib/Servo_motor/Servo_motor.hpp
+++ lib/Servo_motor/Servo_motor.hpp
@@ -3,30 +3,26 @@
 
 #include <Servo.h>
 
-// ERROR CODES
-static const uint8_t SERVO_IS_NOT_ATTACHED = 255;
 
 class Servo_motor : Servo {
 private:
-  Servo servo;
   const std::string uuid;
-  const uint8_t pin;
   const uint16_t poll_rate;
   const uint8_t leftmost_pos_deg;
   const uint8_t rightmost_pos_deg;
+  uint32_t wait;
 public:
-  Servo_motor(const std::string uuid, uint8_t pin, uint16_t poll_rate, uint8_t rightmost_position_degree = 0,
-              uint8_t leftmost_position_degree = 180);
+  Servo_motor(std::string, uint8_t, uint16_t, uint8_t = 0, uint8_t = 180);
 
-  const std::string get_uuid();
-
-  uint8_t get_pin();
+  std::string get_uuid();
 
   uint8_t move_to_rightmost_position();
 
   uint8_t move_to_leftmost_position();
 
   uint8_t get_current_position();
+
+  bool poll();
 };
 
 
